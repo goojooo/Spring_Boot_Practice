c5c108435056efa7e3c0f794c85fc26befd926e5
Nayan <nayandahare02@gmail.com>
2026-03-29 15:52:03 +0530

    F + B integration

--- local-service-platform/src/main/java/com/localservice/controller/HomeController.java
+++ local-service-platform/src/main/java/com/localservice/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping("/")
+	@GetMapping(value = { "/", "/{path:[^\\.]*}" })
 	public String home(){
-	    return "home";
+		return "forward:/index.html";
 	}
 }
